d525966b4f32573ffa87723ffaa450576519eb3f
muzaslivka <ms.dmitrieffa@gmail.com>
2023-05-08 20:33:27 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long result = service.calculate(amount, registered);
+        System.out.println(result);
+
+    }
+}
